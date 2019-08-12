4cd6f92b2ebd0bfe968aa2b660604bb2dcc87754
youvovk <wer3wollf@gmail.com>
2019-08-12 09:40:35 +0300

    add scroll and hovers

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
+window.onscroll = () => scrollFunction();
 
-console.log('hello mate academy!');
+const scrollFunction = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById('button-up_js-scroll-top').style.display = 'block';
+  } else {
+    document.getElementById('button-up_js-scroll-top').style.display = 'none';
+  }
+};
+
+const topBtn = document.getElementById('button-up_js-scroll-top');
+topBtn.addEventListener('click', () =>
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  })
+);
